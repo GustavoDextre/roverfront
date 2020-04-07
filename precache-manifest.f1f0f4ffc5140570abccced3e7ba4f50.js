ddf19b2ea3e61990fb9e3c8f077c2ec6b0baf77e
unknown <gustavodextrezubieta@gmail.com>
2020-04-06 23:03:07 -0500

    Updates

---
+++ precache-manifest.f1f0f4ffc5140570abccced3e7ba4f50.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84e96e607a9f0cb303c60ef505f1846d",
+    "url": "/roverfront/index.html"
+  },
+  {
+    "revision": "f8200f803ecfbc8eaa66",
+    "url": "/roverfront/static/css/2.a0c678d2.chunk.css"
+  },
+  {
+    "revision": "0423263aff3b9213191b",
+    "url": "/roverfront/static/css/main.24193294.chunk.css"
+  },
+  {
+    "revision": "f8200f803ecfbc8eaa66",
+    "url": "/roverfront/static/js/2.226f686a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/roverfront/static/js/2.226f686a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0423263aff3b9213191b",
+    "url": "/roverfront/static/js/main.ef439fbb.chunk.js"
+  },
+  {
+    "revision": "9180d15f4f90baa7244f",
+    "url": "/roverfront/static/js/runtime-main.e33a5b33.js"
+  },
+  {
+    "revision": "6e0b5603fb22690acbfe8ac3ae285391",
+    "url": "/roverfront/static/media/PUMII.6e0b5603.png"
+  },
+  {
+    "revision": "f7e8d222b735973900f6e3e13536a19c",
+    "url": "/roverfront/static/media/UNI.f7e8d222.png"
+  },
+  {
+    "revision": "6be82bd73f10d26cd18690b72546e7a9",
+    "url": "/roverfront/static/media/astronaut.6be82bd7.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/roverfront/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/roverfront/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/roverfront/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/roverfront/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/roverfront/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "ab3f68a78584885eb82d88dc2e999e4b",
+    "url": "/roverfront/static/media/foto1.ab3f68a7.jpg"
+  },
+  {
+    "revision": "241c10acd1b48fd396f0f517f3332966",
+    "url": "/roverfront/static/media/foto10.241c10ac.jpg"
+  },
+  {
+    "revision": "4deb012ed4ef0c18f73e7bccfadc5f19",
+    "url": "/roverfront/static/media/foto2.4deb012e.jpg"
+  },
+  {
+    "revision": "a843f9b799f7ad8245fcaf6a325edc8d",
+    "url": "/roverfront/static/media/foto3.a843f9b7.jpg"
+  },
+  {
+    "revision": "4bd9993f81cc790ab5b56f78675682aa",
+    "url": "/roverfront/static/media/foto4.4bd9993f.jpg"
+  },
+  {
+    "revision": "7f5a81b8bc51332e90f0cb0489f0c492",
+    "url": "/roverfront/static/media/foto5.7f5a81b8.jpg"
+  },
+  {
+    "revision": "19c97a39ce04d257ea1f751aa2918284",
+    "url": "/roverfront/static/media/foto6.19c97a39.jpg"
+  },
+  {
+    "revision": "af17aaa24ff6676803a22707015bbaff",
+    "url": "/roverfront/static/media/foto7.af17aaa2.jpg"
+  },
+  {
+    "revision": "d205ff850d12e803a53330af308dbf79",
+    "url": "/roverfront/static/media/foto8.d205ff85.jpg"
+  },
+  {
+    "revision": "6009dca30c2be7ee124d570554a61acc",
+    "url": "/roverfront/static/media/foto9.6009dca3.jpg"
+  },
+  {
+    "revision": "14144c49e8afddd7c233bad734dd7b4c",
+    "url": "/roverfront/static/media/foto_principal.14144c49.jpg"
+  },
+  {
+    "revision": "ab3f68a78584885eb82d88dc2e999e4b",
+    "url": "/roverfront/static/media/teamPumii.ab3f68a7.jpg"
+  }
+]);
